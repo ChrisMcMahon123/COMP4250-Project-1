9b1699b63f8af217d5e7f08077034aa6ef78059a
ChrisMcMahon123 <chris.mcmahon4@hotmail.com>
2019-02-08 21:31:30 -0500

    Part 1 Done
    Need to do a rewrite to simplify code base and reduce lines of code

--- project1/main.cpp
+++ project1/main.cpp
@@ -4,22 +4,26 @@
 #include <map>
 #include <unordered_set>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    long buckets = 0;// ie number of lines in the file
-
     ifstream infile("retail.txt");
-    string line;
-    const string delimiter = " ";
+
+    if(!infile.is_open())
+    {
+        cout << "Failed to open file" << endl;
+        return 0;
+    }
 
     //FIRST PASSTHROUGH
-    map<long, long> hashmap;
+    const string delimiter = " ";
+    string line;
     size_t pos;
+    map<long, long> hashmap;
     long number;
+    long buckets = 0;// ie number of lines in the file
 
     //read the file line by line
     while (getline(infile, line))
@@ -71,9 +75,8 @@ int main()
     cout << endl;
 
     //SECOND PASSTHROUGH
-    vector<pair<long,long>> tuples;
+    vector<vector<long>> tuples;
     vector<long> tuplesFrequency;
-    pair<long,long> temp;
 
     //generate tuples
     for (auto it1 = hashmap.begin(); it1 != hashmap.end();)
@@ -83,9 +86,7 @@ int main()
         for (auto it2 = it1; it2 != hashmap.end();)
         {
             ++ it2;
-            temp.first = it1->first;
-            temp.second = it2->first;
-            tuples.push_back(temp);
+            tuples.push_back(vector<long>{it1->first, it2->first});
             tuplesFrequency.push_back(0);
         }
     }
@@ -114,18 +115,16 @@ int main()
         while ((pos = line.find(" ")) != string::npos)
         {
             number = stol(line.substr(0, pos));//get the token, convert to long
-
             numbers.insert(number);
-
             line.erase(0, pos + delimiter.length());//get the next number in the line
         }
 
         //check all tuples to see if they appear in the line
         for(unsigned long i = 0; i < tuples.size(); i ++)
         {
-            if(numbers.find(tuples[i].first) != numbers.end())
+            if(numbers.find(tuples[i][0]) != numbers.end())
             {
-                if(numbers.find(tuples[i].second) != numbers.end())
+                if(numbers.find(tuples[i][1]) != numbers.end())
                 {
                     tuplesFrequency[i] ++;
                 }
@@ -137,18 +136,107 @@ int main()
         //cout << token << endl;
     }
 
-    long count = 0;
+    vector<vector<long>> frequent_tuples;
 
     for(unsigned long i = 0; i < tuples.size(); i ++)
     {
         if(tuplesFrequency[i] >= threshold)
         {
-            count ++;
-            cout << "Tuple: " << tuples[i].first << "," << tuples[i].second << " Tuple Frequency: " << tuplesFrequency[i] << endl;
+            frequent_tuples.push_back(tuples[i]);
+            //cout << "Tuple: " << tuples[i][0] << "," << tuples[i][1] << " Tuple Frequency: " << tuplesFrequency[i] << endl;
         }
     }
 
-    cout << "Number of frequent tuples:" << count << endl;
+    for(unsigned long i = 0; i < frequent_tuples.size(); i ++)
+    {
+        cout << "Tuple: " << frequent_tuples[i][0] << "," << frequent_tuples[i][1] << endl;
+    }
+
+    cout << "Number of frequent tuples:" << frequent_tuples.size() << endl;
+
+    //THIRD PASSTHROUGH
+    vector<vector<long>> triplets;
+    vector<long> tripletsFrequency;
+
+    //generate triples
+    for (unsigned long it1 = 0; it1 < frequent_tuples.size(); it1 ++)
+    {
+        for (unsigned long it2 = it1 + 1; it2 < frequent_tuples.size(); it2 ++)
+        {
+            vector<long> val1 = frequent_tuples[it1];
+            vector<long> val2 = frequent_tuples[it2];
+
+            if(val1[0] == val2[0])
+            {
+                if(val1[0] != val2[1])
+                {
+                    tripletsFrequency.push_back(0);
+                    triplets.push_back(vector<long>{val1[0],val1[1],val2[1]});
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    cout << endl << endl;
+
+    for (unsigned long i = 0; i < triplets.size(); i ++)
+    {
+        //cout << "Triplet: " << triplets[i][0] << "," << triplets[i][1] << "," << triplets[i][2] << endl;
+    }
+
+    cout << "Number of triplets:" << triplets.size() << endl;
+
+    numbers.clear();
+    infile.clear();
+    infile.seekg(0, ios::beg);
+
+    //read the file line by line
+    while (getline(infile, line))
+    {
+        //get all numbers in the line
+        pos = 0;
+
+        while ((pos = line.find(" ")) != string::npos)
+        {
+            number = stol(line.substr(0, pos));//get the token, convert to long
+            numbers.insert(number);
+            line.erase(0, pos + delimiter.length());//get the next number in the line
+        }
+
+        //check all tuples to see if they appear in the line
+        for(unsigned long i = 0; i < triplets.size(); i ++)
+        {
+            if(numbers.find(triplets[i][0]) != numbers.end())
+            {
+                if(numbers.find(triplets[i][1]) != numbers.end())
+                {
+                    if(numbers.find(triplets[i][2]) != numbers.end())
+                    {
+                        tripletsFrequency[i] ++;
+                    }
+                }
+            }
+        }
+
+        numbers.clear();
+    }
+
+    long count = 0;
+
+    for (unsigned long i = 0; i < triplets.size(); i ++)
+    {
+        if(tripletsFrequency[i] >= threshold)
+        {
+            cout << "Triplet: " << triplets[i][0] << "," << triplets[i][1] << "," << triplets[i][2] << endl;
+            count ++;
+        }
+    }
+
+    cout << "Number of triplets:" << count << endl;
 
     return 0;
 }
